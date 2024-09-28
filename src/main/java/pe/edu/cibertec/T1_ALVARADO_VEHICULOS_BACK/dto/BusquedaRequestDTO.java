9a80c0c8fa268c13006cb2139cdd557aff3cfb09
jbm08 <jose0803ab@gmail.com>
2024-09-27 21:11:37 -0500

    Agregando dto

---
+++ src/main/java/pe/edu/cibertec/T1_ALVARADO_VEHICULOS_BACK/dto/BusquedaRequestDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.T1_ALVARADO_VEHICULOS_BACK.dto;
+
+public record BusquedaRequestDTO(String placa) {
+}
---
+++ src/main/java/pe/edu/cibertec/T1_ALVARADO_VEHICULOS_BACK/dto/BusquedaResponseDTO.java
@@ -0,0 +1,5 @@
+package pe.edu.cibertec.T1_ALVARADO_VEHICULOS_BACK.dto;
+
+public record BusquedaResponseDTO(String codigo, String mensaje,  String marca, String modelo
+                                  ,String numeroAsientos, String precio, String color) {
+}
